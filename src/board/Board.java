b7a5de7e70146475f12550ba5f373a33eb89c248
Andreas <andreaswerlecolino@gmail.com>
2026-05-18 18:26:06 +0200

    Implémentation de la classe Board et de l'ensemble de ses dépendances conformément au diagramme UML.

---
+++ src/board/Board.java
@@ -0,0 +1,47 @@
+package board;
+
+public class Board {
+    private Slot[] playerSlots;
+    private Slot[] opponentSlots;
+
+    public Board() {
+        playerSlots = new Slot[4];
+        opponentSlots = new Slot[4];
+
+        for (int i = 0; i < 4; i++) {
+            playerSlots[i] = new Slot();
+            opponentSlots[i] = new Slot();
+        }
+    }
+    public Boolean addCard(Card c, int side, int index) // un boolean pour le side plutôt ?
+    {
+        if (index < 0 || index > 3)
+        {
+            return false;
+        }
+        if (side == 0) {
+            if(!playerSlots[index].isEmpty())
+            {
+                return false;
+            }
+            playerSlots[index].setCard(c);
+        }
+        else if (side == 1) {
+            if(!playerSlots[index].isEmpty())
+            {
+                return false;
+            }
+            opponentSlots[index].setCard(c);
+        }
+        else {
+            return false;
+        }
+        return true;
+    }
+    /*
+    public void resolveCombat( Score score)
+    {
+
+    }
+    */
+}
---
+++ src/board/ObstacleCard.java
@@ -0,0 +1,7 @@
+package board;
+
+public class ObstacleCard extends Card {
+    public ObstacleCard(String name, int hp, int maxhp) {
+        super(name, hp, maxhp);
+    }
+}
